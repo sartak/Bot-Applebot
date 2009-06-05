e63fc82708ff403c97932adbe5617f7634d01700
Shawn M Moore <sartak@gmail.com>
2009-06-04 23:50:26 -0400

    Copyright

--- lib/Bot/Applebot.pm
+++ lib/Bot/Applebot.pm
@@ -903,5 +903,12 @@ Bot::Applebot - Arbiter of the word card-based game Apples to Apples
 
 Shawn M Moore, C<sartak@gmail.com>
 
+=head1 COPYRIGHT AND LICENSE
+
+Copyright 2009 Shawn M Moore.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
 =cut
 
